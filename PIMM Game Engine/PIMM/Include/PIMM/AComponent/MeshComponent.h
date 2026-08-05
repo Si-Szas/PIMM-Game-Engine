d13724518d14d0eb9a560a6ec5fdd0b1b379bfa0
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2026-08-05 21:41:25 +0800

    Finished MeshComponent header file

--- PIMM Game Engine/PIMM/Include/PIMM/AComponent/MeshComponent.h
+++ PIMM Game Engine/PIMM/Include/PIMM/AComponent/MeshComponent.h
@@ -1,9 +1,28 @@
 #pragma once
-#include <PIMM/Core/Common.h>
+#include <PIMM/Core/Core.h>
+#include <PIMM/AComponent/AComponent.h>
+#include <vector>
 
 namespace pimm{
-	class MeshComponent
+	class MeshComponent final : public AComponent
 	{
+		pimm_typeid(MeshComponent)
+		
+		public:
+			//CONSTRUCTOR
+			explicit MeshComponent(const AComponentDescriptor& data);
+
+			//GETTER
+			MeshResource* GetMesh() const noexcept;
+			MaterialResource* GetMaterial(ui32 index) const noexcept;
+
+			//SETTER
+			void SetMesh(const RefPtr<MeshResource>& newMesh);
+			void SetMaterial(ui32 index, const RefPtr<MaterialResource>& newMaterial);
+
+		private:
+			RefPtr<MeshResource> m_mesh{};
+			std::vector<RefPtr<MaterialResource>> m_materials{};
 	};
 }
 
